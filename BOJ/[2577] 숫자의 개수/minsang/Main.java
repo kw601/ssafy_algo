68267b8d831ce8e8fc5c6776ea23b9e45b419ac2
Minsang <minsang1233@gmail.com>
2025-08-09 01:25:46 +0900

    day2: BOJ[1546] 평균

--- BOJ/[2577] 숫자의 개수/minsang/Main.java
+++ BOJ/[2577] 숫자의 개수/minsang/Main.java
@@ -1,13 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 class Main{
     static int A, B, C;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
         A = Integer.parseInt(br.readLine());
         B = Integer.parseInt(br.readLine());
